4ceac94641b62e78cf413862030a4ae16a71e2d7
nimishaparayil <nimishakarthika123@gmail.com>
2023-04-10 12:07:06 +0530

    addeded

---
+++ Eshopproject/search_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.db.models import Q
+from Shopstore.models import Product
+
+
+# Create your views here.
+
+def SearchResult(request):
+    products=None
+    query=None
+    if 'q' in request.GET:
+        query=request.GET.get('q')
+        products=Product.objects.all().filter(Q(name__contains=query) | Q(description__contains=query))
+        return render(request,'search.html',{'query':query,'products':products})
+
+
+# Create your views here.
